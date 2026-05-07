29eb22bb9a4726d595509650447bd61bbf00a52d
reachedgauri <reachedgauri@gmail.com>
2026-05-07 11:26:44 +0530

    Delete src/routes/index.tsx

--- src/routes/index.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import WildGuardChat from "@/components/WildGuardChat";
-
-export const Route = createFileRoute("/")({
-  component: WildGuardChat,
-});
